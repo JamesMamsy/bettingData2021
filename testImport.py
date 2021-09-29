b9ed778b5caaf557ba08dca6fe90a853265b2edf
James Mamsy <jpmokc@gmail.com>
2021-09-29 08:53:12 -0500

    Initial Commit -add temp files

---
+++ testImport.py
@@ -0,0 +1,12 @@
+import json
+import requests
+import argparse
+
+
+#Constants 
+API_KEY = "5c086d6702f51ef5502218e32f6f9d3c" #James Millers API Key
+SPORT = 'basketball_nba'
+REGION = 'us'
+MAKRET = 'h2h'
+
+
